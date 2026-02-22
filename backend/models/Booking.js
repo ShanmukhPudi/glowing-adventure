33876414e4c9d62ff6e69be2b8099f3b30635fef
Shanmukh Krishna <Shannu.pudi@outlook.com>
2026-02-22 10:25:43 +0530

    added booking model with User & Hotel ref

---
+++ backend/models/Booking.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const bookingSchema = new mongoose.Schema(
+    {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: [true, "User is required"],
+        },
+
+        hetel: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Hotel",
+            required: [true, "Hotel is required"],
+        },
+
+        roomsBooked: {
+            type: Number,
+            required: [true, "Number of rooms is required"],
+            min: [1, "Please select a room to book"],
+        },
+    },
+);
+
+const Booking = mongoose.model("Booking", bookingSchema);
+
+module.exports = Booking;
